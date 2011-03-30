1e58010a4ee6808f6c0bd2753c1fb7b0040d693a
Olivier Lalonde <olalonde@gmail.com>
2011-03-30 12:02:59 -0400

    added a 2nd notification to js demo

--- js/demo.js
+++ js/demo.js
@@ -4,6 +4,5 @@ var notification = new notify.notification();
 notification.title = "Notification title";
 notification.icon = "emblem-default"; // see /usr/share/icons/gnome/16x16
 notification.send("Notification message");
-
-notification.send("Notification message2");
-
+notification.icon = "terminal"
+notification.send("Second notification... Should be displayed after first one fades out.");
